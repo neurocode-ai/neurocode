8db136b14104ffdbe528afefca54394587c49a69
Eli Vance <wilhelmagren98@gmail.com>
2022-09-10 18:42:54 +0200

    Initial commit, structure of inits and datasets

---
+++ neurocode/datasets/base.py
@@ -0,0 +1,31 @@
+""" --------------------------------------------------------------------
+Copyright [2022] [Wilhelm Ågren]
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+
+File created:   10-09-2022
+Last edited:    10-09-2022
+
+Implementation of the base RecordingDataset and EpochDataset,
+inheriting from the PyTorch dataset object. 
+-------------------------------------------------------------------- """
+import numpy as np
+from torch.utils.data import Dataset
+
+
+class RecordingDataset(Dataset):
+    pass
+
+class EpochDataset(Dataset):
+    pass
