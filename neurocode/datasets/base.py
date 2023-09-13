54fc58c09178170337b36a9aa2fe1d5d7349b0d8
wilhelmagren <wilhelmagren98@gmail.com>
2023-09-13 23:04:42 +0200

    chore(format) reformat files to black codestyle

--- neurocode/datasets/base.py
+++ neurocode/datasets/base.py
@@ -29,30 +29,33 @@ import numpy as np
 
 from torch.utils.data import Dataset
 
+
 class RecordingDataset(Dataset):
     def __init__(self, *args, **kwargs):
         self._setup(*args, **kwargs)
 
     def __len__(self):
-        return self._info['n_recordings']
+        return self._info["n_recordings"]
 
     def __getitem__(self, indices):
         recording, window = indices
         return self._data[recording][window]
-    
+
     def __iter__(self):
         for idx in range(len(self)):
             yield (self._data[idx], self._labels[idx])
-    
+
     def _setup(self, datasets, labels, formatted=False, **kwargs):
         if not formatted:
-            datasets = {recording: dataset for recording, dataset in enumerate(datasets)}
+            datasets = {
+                recording: dataset for recording, dataset in enumerate(datasets)
+            }
             labels = {recording: label for recording, label in enumerate(labels)}
-        
+
         lengths = {recording: len(d) for recording, d in enumerate(datasets.values())}
         info = {
-            'lengths': lengths,
-            'n_recordings': len(datasets),
+            "lengths": lengths,
+            "n_recordings": len(datasets),
         }
         info = {**info, **kwargs}
 
@@ -62,20 +65,20 @@ class RecordingDataset(Dataset):
 
     def get_data(self):
         return self._data
-    
+
     def get_labels(self):
         return self._labels
-    
+
     def get_info(self):
         return self._info
-    
+
     def split(self, ratio=0.6, shuffle=True):
         split_idx = int(len(self) * ratio)
         indices = list(range(len(self)))
 
         if shuffle:
             np.random.shuffle(indices)
-        
+
         train_indices = indices[:split_idx]
         valid_indices = indices[split_idx:]
 
@@ -84,6 +87,10 @@ class RecordingDataset(Dataset):
         X_valid = {idx: self.data[k] for idx, k in enumerate(valid_indices)}
         Y_valid = {idx: self.labels[k] for idx, k in enumerate(valid_indices)}
 
-        train_dataset = RecordingDataset(X_train, Y_train, formatted=True, sfreq=self.info['sfreq'])
-        valid_dataset = RecordingDataset(X_valid, Y_valid, formatted=True, sfreq=self.info['sfreq'])
+        train_dataset = RecordingDataset(
+            X_train, Y_train, formatted=True, sfreq=self.info["sfreq"]
+        )
+        valid_dataset = RecordingDataset(
+            X_valid, Y_valid, formatted=True, sfreq=self.info["sfreq"]
+        )
         return (train_dataset, valid_dataset)
